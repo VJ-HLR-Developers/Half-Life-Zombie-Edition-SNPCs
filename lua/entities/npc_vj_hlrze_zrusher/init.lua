e6ccd52392bc9ce308a8a06587e67e2aab81a15d
DrVrej <DrVrej@users.noreply.github.com>
2026-02-12 10:30:48 +0400

    Cleanup Zombie Rusher's variables

--- lua/entities/npc_vj_hlrze_zrusher/init.lua
+++ lua/entities/npc_vj_hlrze_zrusher/init.lua
@@ -8,81 +8,58 @@ include("shared.lua")
 ENT.Model = "models/vj_hlr/hlze/rusher.mdl"
 ENT.StartHealth = 85
 ENT.HullType = HULL_HUMAN
+ENT.ControllerParams = {
+	ThirdP_Offset = Vector(30, 20, -30),
+	FirstP_Bone = "Bip01 Neck",
+	FirstP_Offset = Vector(2, 0, 8),
+}
 ---------------------------------------------------------------------------------------------------------------------------------------------
 ENT.BloodColor = VJ.BLOOD_COLOR_YELLOW
 ENT.BloodParticle = {"vj_hlr_blood_yellow"}
 ENT.BloodDecal = {"VJ_HLR1_Blood_Yellow"}
 ENT.HasBloodPool = false
 ENT.VJ_NPC_Class = {"CLASS_ZOMBIE"}
+ENT.CanEat = true
+ENT.HasDeathAnimation = true
 
+-- Melee attack
 ENT.HasMeleeAttack = true
-ENT.AnimTbl_MeleeAttack = {ACT_MELEE_ATTACK1}
+ENT.AnimTbl_MeleeAttack = ACT_MELEE_ATTACK1
 ENT.MeleeAttackDamage = 15
-ENT.TimeUntilMeleeAttackDamage = false
 ENT.MeleeAttackDistance = 50
 ENT.MeleeAttackDamageDistance = 80
+ENT.TimeUntilMeleeAttackDamage = false
 
-ENT.HasExtraMeleeAttackSounds = true
-ENT.DisableFootStepSoundTimer = true
-ENT.AnimTbl_Run = {ACT_RUN} -- Set the running animations | Put multiple to let the base pick a random animation when it moves
-ENT.HasDeathAnimation = true
-//ENT.DeathAnimationTime = 0.8
-
-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
------- Leap Attack Variables ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- Leap attack
 ENT.HasLeapAttack = true
 ENT.LeapAttackDamage = 15
-ENT.LeapAttackDamageType = DMG_SLASH -- Type of Damage
-	-- ====== Animation Variables ====== --
+ENT.LeapAttackDamageType = DMG_SLASH
 ENT.AnimTbl_LeapAttack = ACT_GLIDE
-ENT.LeapAttackAnimationDelay = 0 -- It will wait certain amount of time before playing the animation
-ENT.LeapAttackAnimationFaceEnemy = 2 -- 2 = Face the enemy UNTIL it jumps!
-ENT.LeapAttackAnimationDecreaseLengthAmount = 0 -- This will decrease the time until starts chasing again. Use it to fix animation pauses until it chases the enemy.
-	-- ====== Distance Variables ====== --
-ENT.LeapAttackMaxDistance = 2000 -- The distance of the leap
 ENT.LeapAttackMinDistance = 500
+ENT.LeapAttackMaxDistance = 2000
 ENT.LeapAttackDamageDistance = 100
 ENT.LeapAttackAngleRadius = 60
-	-- ====== Timer Variables ====== --
-	-- Set this to false to make the attack event-based:
-ENT.TimeUntilLeapAttackDamage = 1 -- How much time until it runs the leap damage code?
-ENT.TimeUntilLeapAttackVelocity = 0 -- How much time until it runs the velocity code?
-ENT.NextLeapAttackTime = 1.5 -- How much time until it can use a leap attack?
-ENT.NextLeapAttackTime_DoRand = false
-ENT.NextAnyAttackTime_Leap = false -- How much time until it can use any attack again? | Counted in Seconds
-ENT.NextAnyAttackTime_Leap_DoRand = false
-ENT.LeapAttackReps = 1 -- How many times does it run the leap attack code?
-ENT.LeapAttackExtraTimers = nil
-ENT.LeapAttackStopOnHit = true -- Should it stop the leap attack from running rest of timers when it hits an enemy?
-	-- ====== Velocity Variables ====== --
+ENT.TimeUntilLeapAttackDamage = 1
+ENT.TimeUntilLeapAttackVelocity = 0
+ENT.NextLeapAttackTime = 1.5
+ENT.NextAnyAttackTime_Leap = false
+
+-- !!!! Following should be removed in favor of calling it in the leap function !!!! - DrVrej
 ENT.LeapAttackVelocityForward = 300 -- How much forward force should it apply?
 ENT.LeapAttackVelocityUp = 200 -- How much upward force should it apply?
 ENT.LeapAttackVelocityRight = 0 -- How much right force should it apply?
 
 ENT.HasBeforeLeapAttackSound = false
-
-	-- ====== Eating Variables ====== --
-ENT.CanEat = true -- Should it search and eat organic stuff when idle?
-
-	-- ====== NPC Controller Data ====== --
-ENT.ControllerParams = {
-	CameraMode = 1, -- Sets the default camera mode | 1 = Third Person, 2 = First Person
-	ThirdP_Offset = Vector(30, 20, -30),
-	FirstP_Bone = "Bip01 Neck",
-	FirstP_Offset = Vector(2, 0, 8),
-	FirstP_ShrinkBone = true, -- Should the bone shrink? Useful if the bone is obscuring the player's view
-	FirstP_CameraBoneAng = 0, -- Should the camera's angle be affected by the bone's angle? | 0 = No, 1 = Pitch, 2 = Yaw, 3 = Roll
-	FirstP_CameraBoneAng_Offset = 0, -- How much should the camera's angle be rotated by? | Useful for weird bone angles
-}
+ENT.HasExtraMeleeAttackSounds = true
+ENT.DisableFootStepSoundTimer = true
 
 -- Sounds
-ENT.SoundTbl_FootStep = {"vj_hlr/gsrc/npc/roach/rch_walk.wav"}
+ENT.SoundTbl_FootStep = "vj_hlr/gsrc/npc/roach/rch_walk.wav"
 --ENT.SoundTbl_Alert = {"vj_hlr/hlze/zombie/fz_scream1.wav"}
 ENT.SoundTbl_BeforeMeleeAttack = {"vj_hlr/hlze/zombie/zombie_melee2.wav"}
-ENT.SoundTbl_LeapAttackJump = {"vj_hlr/hlze/zombie/leap1.wav"}
 ENT.SoundTbl_MeleeAttackExtra = {"vj_hlr/gsrc/npc/zombie/claw_strike1.wav","vj_hlr/gsrc/npc/zombie/claw_strike2.wav","vj_hlr/gsrc/npc/zombie/claw_strike3.wav"}
 ENT.SoundTbl_MeleeAttackMiss = {"vj_hlr/gsrc/npc/zombie/claw_miss1.wav","vj_hlr/gsrc/npc/zombie/claw_miss2.wav"}
+ENT.SoundTbl_LeapAttackJump = "vj_hlr/hlze/zombie/leap1.wav"
 ENT.SoundTbl_Pain = {"vj_hlr/hlze/zombie/zombie_pain4.wav","vj_hlr/hlze/zombie/zombie_pain5.wav","vj_hlr/hlze/zombie/zombie_pain6.wav","vj_hlr/hlze/zombie/zombie_pain7.wav"}
 ENT.SoundTbl_Death = {"vj_hlr/hlze/zombie/zombie_voice_idle13.wav","vj_hlr/hlze/zombie/zombie_die1.wav","vj_hlr/hlze/zombie/zombie_die2.wav","vj_hlr/hlze/zombie/zombie_die3.wav"}
 
@@ -155,7 +132,6 @@ local gibs1 = {"models/vj_hlr/gibs/agib1.mdl", "models/vj_hlr/gibs/agib2.mdl", "
 function ENT:OnCreateDeathCorpse(dmginfo, hitgroup, corpse)
 	VJ.HLR_ApplyCorpseSystem(self, corpse, gibs1)
 end
-
 ---------------------------------------------------------------------------------------------------------------------------------------------
 function ENT:OnBleed(dmginfo,hitgroup)
 		if self:Health() < 10 && self:Health() > 0 && hitgroup != 1 then
@@ -185,7 +161,6 @@ if self.VJ_IsBeingControlled == true then
 		self.VJ_TheControllerEntity.VJC_Camera_Mode = cameramode -- set the headcrab's view to what ours was set to
 	end )
 end
-		
 end
 
 function ENT:Controller_Initialize(ply, controlEnt)
